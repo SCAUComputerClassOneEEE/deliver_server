78855aa6d132478efe2434bfa47909a5ccddaf51
SCAUBai <13512742057@sina.cn>
2021-04-20 20:48:18 +0800

    插入基本ok（差订单插入）

--- src/main/java/scc1/java_project/deliver_server/dbEntity/Customer.java
+++ src/main/java/scc1/java_project/deliver_server/dbEntity/Customer.java
@@ -6,11 +6,11 @@ import lombok.Data;
 // 客户
 public class Customer {
     private Long customerId;
-    private Long customerPhoneNumber;
     private String customerPassword;
     private String customerName;
     private String city;
     private String street;
     private String detailAddress;
     private String account;
+    private String avatar;
 }
